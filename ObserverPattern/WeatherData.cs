0f61b020ccb7c9fde76f869ff17121249ae9a9ee
yanxi zhu <547110356@qq.com>
2019-10-11 17:07:13 +0800

    add observer pattern project

---
+++ ObserverPattern/WeatherData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public class WeatherData
+    {
+        public double GetTemperature()
+        {
+            return 0f;
+        }
+
+        public double GetHumidity()
+        {
+            return 0f;
+        }
+
+        public double GetPressure()
+        {
+            return 0f;
+        }
+
+        /// <summary>
+        /// 一旦气象测量更新，此方法会被调用
+        /// </summary>
+        public void MeasurementsChanged()
+        {
+        }
+    }
+}
